ec7f136f8d9a4356b5d30f91d623ec7919c7c275
Jagat Nandan Prasad <62509653+jagatabhay@users.noreply.github.com>
2020-08-17 17:39:28 +0530

    Update session5.py

--- session5.py
+++ session5.py
@@ -6,13 +6,13 @@ def time_it(fn, *args, repetitons= 1, **kwargs):
 
     if fn == 'temp_converter':
         start_time = time.time()
-        for _ in range( repetitons )
+        for i in range( repetitons )
             temp_converter(args)
         end_time = time.time()
         return (end_time-start_time)/repetitons
     elif fn == 'polygon_area':
         start_time = time.time()
-        for _ in range( repetitons )
+        for i in range( repetitons )
             temp_converter(args)
         end_time = time.time()
         return (end_time-start_time)/repetitons
